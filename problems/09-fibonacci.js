e5eae2ba2446518594c574ad840eb6cf34959ff9
Scott Feichter <scottfeichter@gmail.com>
2024-01-23 17:22:08 -0800

    11 in progress

--- problems/09-fibonacci.js
+++ problems/09-fibonacci.js
@@ -12,6 +12,7 @@ examples below if you're unsure where to start!
 
 Examples:
 
+fibonacci(0); // 0
 fibonacci(1); // 1
 fibonacci(2); // 1
 fibonacci(3); // 2
@@ -20,9 +21,11 @@ fibonacci(10); // 55
 ***********************************************************************/
 
 function fibonacci(n) {
-  // Your code here 
+  if (n === 1 || n === 2) return 1;
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
-  
+
+console.log(fibonacci(10)); // 55
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = fibonacci;
-  
