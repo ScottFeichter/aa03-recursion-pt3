0209903884350124fa0b3fb3dc60a644a36929b2
Scott Feichter <scottfeichter@gmail.com>
2024-01-24 10:35:06 -0800

    11 finished

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -12,11 +12,34 @@ flatten([1, 2]); // [1, 2]
 flatten([1, [2, [3]]]); // [1, 2, 3]
 ***********************************************************************/
 
+let flatten = (arr) => {
+  debugger;
+  let unpacked = [];
 
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      unpacked.push(...flatten(arr[i]));
+    } else {
+      unpacked.push(arr[i]);
+    }
+  }
+  return unpacked;
+};
 
-console.log(flatten([])); // []
-console.log(flatten([1, 2])); // [1, 2]
-console.log(flatten([1, [2, [3]]])); // [1, 2, 3]
+// console.log(flatten([])); // []
+// console.log(flatten([1, 2])); // [1, 2]
+// console.log(flatten([1, [2, [3]]])); // [1, 2, 3]
+// console.log(flatten([[[[]]]])); // []
+
+console.log(
+  flatten([
+    [1, 2],
+    [3, [4, 5]],
+  ])
+); //  [1, 2, 3, 4, 5]
+
+// assert.deepEqual(flatten([[[1, 2]], [[3, 4]]]), [1, 2, 3, 4]);
+// });
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = flatten;
