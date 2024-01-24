78f0ce27eeb68e230794d7dd1d1c9709c7c5c310
Ruben Cuello <kuelmusic@gmail.com>
2024-01-24 13:57:33 -0500

    55

--- problems/12-is-sorted.js
+++ problems/12-is-sorted.js
@@ -14,15 +14,17 @@ isSorted([5, 4, 3, 2, 1]); // false
 function isSorted(arr) {
 
 
-
+// this conditional checks if the first element of the arr is less than the second and if its not it returns false
   if(arr[0] > arr[1]){
     return false
   }
+
+  // this conditional checks if there is only 1 element in the arr and if there is it returns true
   if(arr.length === 1){
     return true
   }
 
-
+  // this conditional checks if first ele is less than the second and if it is it removes it and calls the function 
   if(arr[0] <= arr[1]) {
     arr.shift()
     // console.log(arr)
